a9b99beb389c1e96acea0fa2c4bef474dd1edb15
khaja2mca <khaja2mca@gmail.com>
2020-11-11 11:04:19 +0530

    added new file SAK.txt

--- JavaGit.java
+++ JavaGit.java
@@ -1,8 +1,11 @@
-public class JavaDemo {
-    public static void main(String[] args) {
-    	for(int i=1;i<=10;i++){
+public class JavaGit
+{
+    public static void main(String[] args) 
+ {
+    	for(int i=1;i<=10;i++)
+	{
 
-        System.out.println("Git and GitHub" +i);   
-    		}
-	}
+        	System.out.println("Git and GitHub" +i);   
+    	}
+ }
 }
